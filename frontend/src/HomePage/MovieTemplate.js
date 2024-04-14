95d80fe4002d6d6225e09f1549fbb246995b9736
kunj bhesaniya <kcbhesaniya10@gmail.com>
2024-04-14 15:12:02 +0530

    ADD : movie template completed

--- frontend/src/HomePage/MovieTemplate.js
+++ frontend/src/HomePage/MovieTemplate.js
@@ -1,19 +1,21 @@
+import img from '../MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg'
 const MovieTemplate = () => {
     return ( 
         <div className= "mx-5">
             <div className=" movie-description row justify-content-sm-around">
-                <div className="col-2">
-                    <img src="./logo192.png" style={{height: '100px'}}></img>
+                <div className="col-2 justify-content-center d-flex align-items-center">
+                    <img src={img} style={{height: '150px'}} ></img>
                 </div>
-                <div className="col bg-warning me-3">
-                        <div className="container movie-title">Movie-title</div>
-                        <div className="row">
-                            <ul>
-                                <li>year of release</li>
-                                <li>duration</li>
-                                <li>certification</li>
+                <div className="col d-flex flex-column justify-content-center">
+                    <div>
+                        <div className="container movie-title  fs-4">Movie-title</div>
+                            <ul className="list-group-horizontal d-flex" style={{paddingLeft: '1rem'}}>
+                                <li className="d-inline-block me-2">year of release</li>
+                                <li className="d-inline-block me-2">duration</li>
+                                <li className="d-inline-block me-2">certification</li>
                             </ul>
-                        </div>
+                        <div className="container IMDB-rating">IMDB: 9.0</div>
+                      </div>
                 </div>
             </div>
         </div>
