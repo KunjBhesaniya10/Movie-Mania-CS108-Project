39a0364c4ae03c0e59013e4e17df94615b2c70b0
kunj bhesaniya <kcbhesaniya10@gmail.com>
2024-04-17 19:48:29 +0530

    BACKEND - User Model created succesfully and added in user interface of django

--- backend/MovieData/models.py
+++ backend/MovieData/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
 # Create your models here.
+class users(models.Model):
+    username= models.CharField(primary_key=True,max_length=20)
+    password = models.CharField(max_length=10)
+    def __str__(self) -> str:
+        return self.username
+    class Meta:
+        verbose_name_plural = 'users'
